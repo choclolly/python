d9abf2d52393196da11441202f03aa03fbaf34b4
haodonghui <haodonghui@yestae.com>
2020-12-22 11:43:41 +0800

    py3 读写csv文件

--- learning/py3/读取csv/读.py
+++ learning/py3/读取csv/读.py
@@ -1,12 +1,17 @@
 # https://www.cnblogs.com/superhin/p/11495956.html
 import csv
+from itertools import islice
 
-import csv
+# with open('result.csv', encoding='utf-8') as f:
+#     reader = csv.reader(f)
+#     for row in islice(reader, 1, None):
+#         print(row)
 
-with open('result.csv', encoding='utf-8') as f:
-    next(f)
+'''
+    读入csv跳过表头
+        使用islice即可
+    '''
+with open('d://result.csv', encoding='utf-8') as f:
     reader = csv.reader(f)
-    header = next(reader)
-    print(header)
-    for row in reader:
+    for row in islice(reader, 1, None):
         print(row)
